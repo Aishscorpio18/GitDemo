4e3cb82062df3c73f75c5a8a6aaa9d070181c0c7
Aishwarya <aishdhilip@gmail.com>
2021-07-30 13:24:51 -0400

    first commit

---
+++ src/test/java/qaclickacademy/ApiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiumTest {
+
+		@Test
+		public void NativeApps()
+		{
+			System.out.println("NativeApps");
+		}
+
+	 @Test
+	 public void IOSApps()
+	 {
+		 System.out.println("IOSApps");
+	 }
+
+}
